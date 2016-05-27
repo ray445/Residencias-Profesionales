e22d33b0cc46e67cf1e9984d120b0b4852b15a56
ray445 <raymundo.hernandezfernandez@sistemas.tecsanpedro.edu.mx>
2016-05-26 19:19:15 -0500

    creacion de las tablas
    se han creado las tablas Alumnos y Carreras

--- Base de Datos/Script BD.sql
+++ Base de Datos/Script BD.sql
@@ -1,1 +1,19 @@
 create database Residencias
+
+--nombre, apellido, matricula, telefono, correo, carrera, sexo
+create table Alumnos ( 
+matricula varchar(10) primary key,
+nombre varchar(30) not null,
+apPaterno varchar(30) not null,
+apMaterno varchar(30) not null,
+telefono varchar(13) null,
+correo varchar(100) not null,
+cve_carrera varchar(50) not null,
+sexo char(1) not null,
+)
+
+create table Carreras(
+cve_carrera varchar(10) primary key,
+nombre varchar(50) not null unique
+)
+
