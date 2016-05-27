c7b21e6cacf770c3f95b62d5e20af41e0357862b
ray445 <raymundo.hernandezfernandez@sistemas.tecsanpedro.edu.mx>
2016-05-27 16:48:00 -0500

    solucion en visual studio

---
+++ Residencias Profesionales/Residencias Profesionales/Metodos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace Residencias_Profesionales
+{
+    class Metodos
+    {
+        private string cadena = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+        SqlConnection conexion;
+
+        //metodo para abrir conexion
+        private SqlConnection Conectar()
+        {
+
+                conexion = new SqlConnection(cadena);
+                conexion.Open();
+                return conexion;
+        }
+        //metodo para cerrar conexion
+        public void Desconcetar()
+        {
+                conexion.Close();
+        }
+    }
+}
