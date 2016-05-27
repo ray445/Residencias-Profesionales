199ab650351aeb4ece4be50aa3130ba133a7faed
ray445 <raymundo.hernandezfernandez@sistemas.tecsanpedro.edu.mx>
2016-05-26 20:27:44 -0500

    Agregar tablas
    se han agregado las tablas que le correspondian a Rocha

--- Base de Datos/Script BD.sql
+++ Base de Datos/Script BD.sql
@@ -1,6 +1,5 @@
 create database Residencias
 
---nombre, apellido, matricula, telefono, correo, carrera, sexo
 create table Alumnos ( 
 matricula varchar(10) primary key,
 nombre varchar(30) not null,
@@ -17,3 +16,16 @@ cve_carrera varchar(15) primary key,
 nombre varchar(50) not null unique
 )
 
+create table Usuarios(
+NombreUser varchar(30) not null,
+Contraseña varchar(30) not null,
+Cargo varchar(30) not null
+)
+
+create table FechasRev(
+Fecha1 date,
+Fecha2 date,
+Fecha3 date,
+Fecha4 date,
+Fecha5 date
+)
