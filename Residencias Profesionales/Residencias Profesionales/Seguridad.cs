72ceb6cf48c8fae8e1eae47940f52011cb13c65a
ray445 <raymundo.hernandezfernandez@sistemas.tecsanpedro.edu.mx>
2016-05-27 17:03:45 -0500

    Seguridad
    clase para implementar encriptaciones, usado para las contraseñas

---
+++ Residencias Profesionales/Residencias Profesionales/Seguridad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Residencias_Profesionales
+{
+    class Seguridad
+    {
+        public static string encriptar(string palabra)
+        {
+            string res = string.Empty;
+            byte[] encripta = System.Text.Encoding.Unicode.GetBytes(palabra);
+            res = Convert.ToBase64String(encripta);
+            return res;
+        }
+
+        public static string desencriptar(string palabra)
+        {
+            string result = string.Empty;
+            byte[] desencript = Convert.FromBase64String(palabra);
+            result = Convert.ToBase64String(desencript);
+            return result;
+        }
+    }
+}
