101cc7be797e27b513781cf153e98cfa4ab38cd0
ray445 <raymundo.hernandezfernandez@sistemas.tecsanpedro.edu.mx>
2016-05-27 17:48:45 -0500

    SP de acceso

---
+++ Base de Datos/SP_Acceso.sql
@@ -0,0 +1,18 @@
+
+
+create procedure sp_Acceso 
+@nombre varchar(30), @contraseña varchar(60)
+as
+begin 
+	select NombreUser as Nombre, Contraseña from Usuarios
+	where NombreUser=@nombre and @contraseña=Contraseña
+end
+
+--exec sp_Acceso 'Raymundo','patoganzo'
+
+/*
+insert into Usuarios values('Raymundo','patoganzo','Asesor','A-001')
+
+insert into Asesores values('A-001','Raul','Hdz','Fdz','asdsad','123123123','C-001')
+
+insert into Carreras values('C-001','Sistemas Computacionales')*/
