30ce9f308cac23a45f29e6dbbc8c1c2a76410179
ray445 <raymundo.hernandezfernandez@sistemas.tecsanpedro.edu.mx>
2016-05-29 15:36:57 -0500

    Update sp_insertarAsesor.sql

--- Base de Datos/sp_insertarAsesor.sql
+++ Base de Datos/sp_insertarAsesor.sql
@@ -1,4 +1,4 @@
-create sp_insertarAsesores proc
+create proc sp_insertarAsesores 
 @cveAsesor varchar ( 10 ),@nombre varchar ( 30 ),@apPaterno varchar ( 30 ),
 @apMaterno varchar ( 30 ),@correo varchar ( 60 ),
 @telefono varchar ( 13 ),@cve_carrera varchar ( 15 )
@@ -7,12 +7,11 @@ as
 begin
 	begin try
 		begin tran t
-		insert  Asesores values (@ cveAsesor, nombre @, @ apPaterno, apMaterno @, @ Correo, telefono @, @ cve_carrera)
+		insert  into Asesores values (@cveAsesor, @nombre , @apPaterno, @apMaterno , @correo, @telefono, @cve_carrera)
 		commit tran t
 	End try
 		begin catch
 		rollback tran t
 		end catch
 end
-
 
