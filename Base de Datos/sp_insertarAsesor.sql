e6aee5315bf0aa1fc6bd251a4e7a699de57c8a82
consuelo1 <consuelo.menavasquez@sistemas.tecsanpedro.edu.mx>
2016-05-28 20:25:55 -0500

    SP_insertarAsesor

---
+++ Base de Datos/sp_insertarAsesor.sql
@@ -0,0 +1,18 @@
+create sp_insertarAsesores proc
+@cveAsesor varchar ( 10 ),@nombre varchar ( 30 ),@apPaterno varchar ( 30 ),
+@apMaterno varchar ( 30 ),@correo varchar ( 60 ),
+@telefono varchar ( 13 ),@cve_carrera varchar ( 15 )
+
+as
+begin
+	begin try
+		begin tran t
+		insert  Asesores values (@ cveAsesor, nombre @, @ apPaterno, apMaterno @, @ Correo, telefono @, @ cve_carrera)
+		commit tran t
+	End try
+		begin catch
+		rollback tran t
+		end catch
+end
+
+
