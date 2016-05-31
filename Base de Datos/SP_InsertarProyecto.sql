ee9e8d9ba4ab9febcd392cfc8b51141146a0ac0f
SRGIISC <saul.rochagomez@sistemas.tecsanpedro.edu.mx>
2016-05-30 20:26:47 -0500

    Crear instrucciones DML para agregar el proyecto del alumno

---
+++ Base de Datos/SP_InsertarProyecto.sql
@@ -0,0 +1,29 @@
+
+
+use Residencias
+
+create procedure sp_InsertarProyecto
+@matriculaAlumno varchar(10),
+@nombreProyecto varchar(50),
+@nombreEmpresa varchar(50),
+@calificacion int,
+@cveAsesor varchar(10),
+@cveRevisor1 varchar(10),
+@cveRevisor2 varchar(10), 
+@periodo varchar(45),
+@inicio date, 
+@fin date
+as	
+	begin
+		begin try 
+			begin tran t
+				insert into Proyectos(matriculaAlumno, nombreEmpresa, nombreProyecto, calificacion, cveAsesor,
+				 cveRevisor1, cveRevisor2, periodo, inicio, fin)
+				values(@matriculaAlumno,@nombreEmpresa,@nombreProyecto,@calificacion,
+				@cveAsesor,@cveRevisor1,@cveRevisor2,@periodo,@inicio,@fin)
+			commit tran t
+		end try
+		begin catch
+			rollback tran t
+		end catch
+	end
