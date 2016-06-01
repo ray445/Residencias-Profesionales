fd8eaab607069b673d30e9a8bacfb125872931b8
ray445 <raymundo.hernandezfernandez@sistemas.tecsanpedro.edu.mx>
2016-06-01 16:07:44 -0500

    modificacion

--- Base de Datos/sp_updateproyecto.sql
+++ Base de Datos/sp_updateproyecto.sql
@@ -1,20 +1,22 @@
 
 
 
-use Residencias
 create proc sp_updateproyecto
  @calificacion int,
- @folioProyecto int
+ @folioProyecto int,
+ @msj varchar(100) output
 as
 begin
 	begin try
 		begin tran t
 		UPDATE Proyectos
-		SET calificacion = [@calificacion]
-		WHERE folioProyecto = [@folioProyecto]
+		SET calificacion = @calificacion
+		WHERE folioProyecto = @folioProyecto
 		commit tran t
+		set @msj='Datos Agregados'
 	end try
 	begin catch	
 		rollback tran t
+		set @msj='Error al cargar los datos'
 	end catch	
 end
