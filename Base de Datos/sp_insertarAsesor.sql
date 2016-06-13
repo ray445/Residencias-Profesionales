13844ddca9b1711c7049d849a9a79eef72221d09
consuelo1 <consuelo.menavasquez@sistemas.tecsanpedro.edu.mx>
2016-06-13 16:52:46 -0500

    Update sp_insertarAsesor.sql

--- Base de Datos/sp_insertarAsesor.sql
+++ Base de Datos/sp_insertarAsesor.sql
@@ -18,3 +18,6 @@ begin
 end
 
 select * from asesores
+
+----este SP vamos a insertar las datos siguentes para para la tabla asesores tambien mostramos
+----un mensaje donde los datos ya an sido agregados en la tabla
